088bb93654349ef1d7b65cea3854904e5ad20f99
BhuvanDev <bhuvanraj262005@gmail.com>
2024-12-02 12:59:12 +0530

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,10 +1,13 @@
 import {cart, addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let productsHTML = ''; 
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
+function renderProductsGrid() {
+  let productsHTML = ''; 
+
+  products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
             <div class="product-image-container">
@@ -13,7 +16,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-name limit-text-to-2-lines">
-           ${product.name}
+            ${product.name}
             </div> 
 
             <div class="product-rating-container">
@@ -59,27 +62,28 @@ products.forEach((product) => {
             </button>
         </div>
         `; 
-}); 
+  }); 
 
-document.querySelector('.js-products-grid').
-innerHTML = productsHTML;
+  document.querySelector('.js-products-grid').
+  innerHTML = productsHTML;
 
-function updateCartQuantity() {
+  function updateCartQuantity() {
   let cartQuantity = 0;
 
   cart.forEach((cartItem) => {
-     cartQuantity += cartItem.quantity;
+      cartQuantity += cartItem.quantity;
   });
 
   document.querySelector('.js-cart-quantity')
   .innerHTML = cartQuantity;
-}
+  }
 
-document.querySelectorAll('.js-add-to-cart')
+  document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-       const productId = button.dataset.productId;
-       addToCart(productId);
-       updateCartQuantity();
-     });
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
+      });
   });
+}
