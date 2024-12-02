3e8b4e7ec1fd6644a18ebe49ad2cd531ebf27796
BhuvanDev <bhuvanraj262005@gmail.com>
2024-12-02 20:22:42 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -105,6 +105,8 @@ export function loadProductsFetch() {
     });
   
     console.log('load products');
+  }).catch((error) => {
+    console.log('unexpected error. please try again later.');
   });
 
   return promise;
@@ -129,6 +131,10 @@ export function loadProducts(fun) {
   console.log('load products');
 
   fun();
+ });
+
+ xhr.addEventListener('error', (error) => {
+  console.log('unexpected error. please try again later.');
  });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
